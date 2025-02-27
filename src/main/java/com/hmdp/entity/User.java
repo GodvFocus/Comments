32e4cd8f5104f5b1184437e77b95af7790c010d9
Parker <1083835025@qq.com>
2025-02-27 17:20:07 +0800

    添加了注释

--- src/main/java/com/hmdp/entity/User.java
+++ src/main/java/com/hmdp/entity/User.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@TableName("tb_user")
+@TableName("tb_user") // 指定该实体类对应的数据库表名
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
