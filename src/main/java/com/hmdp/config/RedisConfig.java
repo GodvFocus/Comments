0fea889f3307aca7dd4b06519219a9f23b53030e
Parker <1083835025@qq.com>
2025-03-17 14:53:22 +0800

    对新版本Redis进行相应修改

--- src/main/java/com/hmdp/config/RedisConfig.java
+++ src/main/java/com/hmdp/config/RedisConfig.java
@@ -11,7 +11,8 @@ public class RedisConfig {
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
+//        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         return Redisson.create(config);
     }
 }
